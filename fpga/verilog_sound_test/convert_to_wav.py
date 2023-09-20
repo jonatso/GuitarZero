141156b0468e66ff1f319cccb31f1a19fe19b650
jonatso <jonatan.solheim@ntnu.no>
2023-09-20 18:25:18 +0200

    multiple working generators

--- fpga/verilog_sound_test/convert_to_wav.py
+++ fpga/verilog_sound_test/convert_to_wav.py
@@ -3,7 +3,7 @@ from scipy.io.wavfile import write
 
 # Read sound data from the file
 with open('sound_data.txt', 'r') as f:
-    data = [int(line.strip()) // 2 for line in f.readlines()]
+    data = [int(line.strip()) - 32768 for line in f.readlines()]
 
     # Convert the data to a numpy array
     data = np.array(data, dtype=np.int16)
