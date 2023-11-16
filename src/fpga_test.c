a5aa179ea5d12718cefa9548ef65e09fc8674d22
stianjsu <stianjsu@gmail.com>
2023-11-16 16:36:35 +0100

    fix: wierd include error in cmake project

--- src/fpga_test.c
+++ src/fpga_test.c
@@ -2,8 +2,9 @@
 #include "mini_midi.h"
 #include "mini_midi_transfer.h"
 #include "all_star.h"
-#include "level_render.h"
 #include "led_matrix.h"
+#include "level_render.h"
+#include "app.h"
 
 int start_time = 0;
 void init_fpga_test()
@@ -16,5 +17,7 @@ void progress_fpga_test()
 {
     int current_time = get_time_in_milliseconds() - start_time;
     send_notes(song, current_time);
+#if PRODUCTION_BUILD
     render_level(song, progress_in_sixteenths(song, current_time), LED_MATRIX_WIDTH, LED_MATRIX_HEIGHT, pixels);
+#endif
 }
